51a10269f16be657ecfef9bc25b3f0f7ce3a2f1f
Jorge Gabriel <jorgegabriel1993@hotmail.com>
2022-03-09 05:01:22 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/ssrdsmovie/SsrDsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ssrdsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsrDsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsrDsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/ssrdsmovie/SsrDsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ssrdsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsrDsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
