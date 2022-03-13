8a4bf2aa92c55bed4a449013f41ba7c338170451
Jorge Gabriel <jorgegabriel1993@hotmail.com>
2022-03-13 12:06:30 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -2,15 +2,17 @@ import MovieStars from "../MovieStars";
 import './styles.css';
 
 
-    const score = 3.5;
-    const count = 13;
+type Props ={
+    score: number;
+    count:number;
+}
 
-function MovieScore() {
+function MovieScore( { score, count } : Props) {
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
