05aab1b9d3e4ccd880ba9710ec3ec1afa1d2f5a7
Jorge Gabriel <jorgegabriel1993@hotmail.com>
2022-03-13 12:22:05 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -31,12 +31,14 @@ function Listing() {
 
     }, [pageNumber]);
 
-
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
